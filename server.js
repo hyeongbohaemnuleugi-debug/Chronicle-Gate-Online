a7543fc04db19ce02c40d4917ec5db9aca096e9d
hyeongbohaemnuleugi-debug <hyeongbohaemnuleugi@gmail.com>
2026-08-08 09:00:05 +0900

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = new Server(server, {
   maxHttpBufferSize: 100_000,
 });
 const PORT = Number(process.env.PORT || 3000);
-const APP_VERSION = '4.2.0-rc.1';
+const APP_VERSION = '4.3.2-rc.1';
 const MAX_PLAYERS = 4;
 const MIN_PLAYERS = 1;
 const TARGET_STORY = 20;

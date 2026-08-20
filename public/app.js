b098396c6b150facb89b6ae356ce281814786ba3
Chronicle Gate Online <hyeongbohaemnuleugi@gmail.com>
2026-08-20 14:59:36 +0900

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
-import { DiceTheater } from './dice3d.js?v=8360';
+import { DiceTheater } from './dice3d.js?v=8370';
 
-const CLIENT_BUILD = '8.3.0-dice-visual-8360';
+const CLIENT_BUILD = '8.3.0-dice-visual-8370';
 console.info(`[Chronicle Gate] client ${CLIENT_BUILD}`);
 
 const socket = window.io({ timeout: 10_000, reconnection: true, reconnectionAttempts: Infinity, reconnectionDelay: 500, reconnectionDelayMax: 5_000 });
